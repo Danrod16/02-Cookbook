b6d379d0db3729c63437dd2bfe657a0ef036a359
Daniel Rodriguez <daniel.rod93@gmail.com>
2021-08-02 19:02:19 +0200

    Fix: added cookbook day 2

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,5 +1,6 @@
 require_relative 'view'
 require_relative 'recipe'
+require_relative 'scrape_service'
 
 
 class Controller
@@ -9,10 +10,20 @@ class Controller
   end
   #CRUD
   def create
+    # 1. Ask user for a name (view)
     name = @view.ask_user("name")
+    # 2. Ask user for a description (view)
     description = @view.ask_user("description")
-    recipe = Recipe.new(name, description)
-    @cookbook.add(recipe)
+    # 3. Ask user for rating (view)
+    rating = @view.ask_user("rating")
+    # 4. Ask user for prep time (view)
+    prep_time = @view.ask_user("prep_time")
+    # 5. Create recipe (model)
+    recipe = Recipe.new(name: name, description: description, rating: rating, prep_time: prep_time)
+    # 6. Store in cookbook (repo)
+    @cookbook.add_recipe(recipe)
+    # 7. Display
+    display_recipes
   end
 
   def list
@@ -23,6 +34,32 @@ class Controller
     display_recipes
     recipe = @view.ask_user("index")
     @cookbook.remove(recipe.to_i - 1)
+  end
+
+  def import
+    # 1. Ask user for a keyword
+    term = @view.ask_user("search")
+    # 2. Scrape All Recipes
+    results = ScrapeService.new(term).scrape
+    # 3. Display results
+    @view.display(results)
+    # 4. Ask for the recipe to import
+    index = @view.ask_user("index").to_i
+    # 5. Add to cookbook
+    @cookbook.add_recipe(results[index])
+    # 6. Display
+    display_recipes
+  end
+
+  def mark_as_done
+    # 1. Display recipes
+    display_recipes
+    # 2. Ask user for an index (view)
+    index = @view.ask_user("index").to_i
+    # 3. Mark as done and save (repo)
+    @cookbook.mark_recipe_as_done(index - 1 )
+    # 4. Display recipes
+    display_recipes
   end
 
   private
--- lib/cookbook.rb
+++ lib/cookbook.rb
@@ -12,7 +12,7 @@ class Cookbook
     @recipes
   end
 
-  def add(recipe)
+  def add_recipe(recipe)
     @recipes << recipe
     save_to_csv
   end
@@ -22,19 +22,27 @@ class Cookbook
     save_to_csv
   end
 
+  def mark_recipe_as_done(index)
+    recipe = @recipes[index]
+    recipe.mark_as_done!
+    save_to_csv
+  end
+
   private
 
-  def load_csv
-    CSV.foreach(@csv_file) do |recipe|
-      @recipes << Recipe.new(recipe[0], recipe[1])
+  def save_to_csv
+    CSV.open(@csv_file, "wb") do |csv|
+      csv << ["name", "description", "rating", "prep_time", "done"]
+      @recipes.each do |recipe|
+        csv << [recipe.name, recipe.description, recipe.rating, recipe.prep_time, recipe.done?]
+      end
     end
   end
 
-  def save_to_csv
-    CSV.open(@csv_file, 'wb') do |csv|
-      @recipes.each do |recipe|
-       csv << [recipe.name, recipe.description]
-      end
+  def load_csv
+    CSV.foreach(@csv_file, headers: :first_row, header_converters: :symbol) do |row|
+      row[:done] = row[:done] == "true"
+      @recipes << Recipe.new(row)
     end
   end
 end
